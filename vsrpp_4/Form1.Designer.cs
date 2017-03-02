531354b882f0d52d0c86009b81430aee17236d77
RomanHalaburda <mtbrider94@gmail.com>
2017-03-03 02:03:11 +0300

    add some interface and logic

--- vsrpp_4/Form1.Designer.cs
+++ vsrpp_4/Form1.Designer.cs
@@ -28,21 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDoSearch = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(6, 19);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Select folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // btnDoSearch
+            // 
+            this.btnDoSearch.Location = new System.Drawing.Point(6, 19);
+            this.btnDoSearch.Name = "btnDoSearch";
+            this.btnDoSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnDoSearch.TabIndex = 2;
+            this.btnDoSearch.Text = "Do search";
+            this.btnDoSearch.UseVisualStyleBackColor = true;
+            this.btnDoSearch.Click += new System.EventHandler(this.btnDoSearch_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSelectFolder);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnDoSearch);
+            this.groupBox2.Location = new System.Drawing.Point(12, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(487, 366);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDoSearch;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- vsrpp_4/Form1.cs
+++ vsrpp_4/Form1.cs
@@ -25,8 +25,13 @@ namespace vsrpp_4
             процессе работы приложения вести текстовый файл с результатами работы. 
          */
 
+        private String folder = null;
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            groupBox2.Enabled = false;
+
+            /*
             string a = @"C:\Каталог 1\Каталог 2\Каталог 3\Файл.exe";
             if (Path.HasExtension(a))
             {
@@ -40,25 +45,336 @@ namespace vsrpp_4
             MessageBox.Show(File.GetAttributes(@"C:\Windows").ToString());
             MessageBox.Show(File.GetAttributes(@"C:\Windows\notepad.exe").ToString()); 
 
-
-
-
             string с = @"C:\Каталог 1\Каталог 2\Каталог 3\Файл.exe";
             string p = Path.GetDirectoryName(с);
             MessageBox.Show(p); 
-
-
 
             string b = @"C:\Каталог 1\Каталог 2\Каталог 3\Файл.exe";
             string fileName = Path.GetFileNameWithoutExtension(b);
             string fileExtension = Path.GetExtension(b);
             MessageBox.Show(String.Format("Путь:  {0}\r\nИмя  файла: {1}\r\nТип файла: {2}", b, fileName, fileExtension)); 
-        
-            
 
-            MessageBox.Show(String.Format("Путь  к  временному  каталогу: {0}", Path.GetTempPath())); 
+            MessageBox.Show(String.Format("Путь  к  временному  каталогу: {0}", Path.GetTempPath()));
+            */
+
+            
         
-        
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog1.FileName, Encoding.UTF8);
+                folder = openFileDialog1.FileName;
+                label1.Text = Path.GetFileName(openFileDialog1.FileName);
+                //sb1.Append(sr.ReadToEnd());
+                sr.Close();
+
+                groupBox2.Enabled = true;
+            }
+        }
+
+        private void btnDoSearch_Click(object sender, EventArgs e)
+        {
+            string[] findFiles = System.IO.Directory.GetFiles(@label1.Text, "*.jpg", System.IO.SearchOption.AllDirectories);
         }
     }
 }
+
+/*
+ private async void findFiles()
+        {
+            await Task.Run(() =>
+            {
+                Action<string> addInListAction = (string text) =>
+                {
+                    lb.Items.Add(text);
+                    tb.Text = "В списке: " + lb.Items.Count.ToString();
+                };
+                SearchFilesInDirectory("D:\\", new List<string>() { "*.jpg", "*.png" }, addInListAction);
+            });
+        }
+        /// <summary>
+        /// Поиск файлов по маске
+        /// </summary>
+        /// <param name="directory">Директория, в которой осуществляется поиск</param>
+        /// <param name="searchPatternList">
+        /// Строка поиска, которую необходимо сравнивать с именами файлов в directory. Параметр 
+        ///     не должен заканчиваться двумя точками ("..") или содержать две точки ("..") 
+        ///     непосредственно перед System.IO.Path.DirectorySeparatorChar или System.IO.Path.AltDirectorySeparatorChar, 
+        ///     а также не должен содержать символы, входящие в список System.IO.Path.InvalidPathChars.
+        /// </param>
+        /// <param name="method">Делегат метода, который принимает параметр типа System.String помещен в очередь событий System.Windows.Threading.Dispatcher.</param>
+        /// <exception cref="ArgumentException">
+        /// <para>Путь является строкой нулевой длины, содержит только пробелы или содержит недопустимые символы.</para>
+        /// <para>Параметр поиска не является допустимым значением.</para>
+        /// </exception>
+        /// <exception cref="DirectoryNotFoundException">Путь недопустим, например, ссылается на неподключенный диск.</exception>
+        /// <exception cref="IOException">
+        /// <para>Путь является именем файла.</para>
+        /// <para>Длина указанного пути, имени файла или обоих параметров превышает установленный системой предел.</para>
+        /// </exception>
+        /// <exception cref="UnauthorizedAccessException">Исключение, возникающее в случае запрета доступа операционной системой из-за 
+        /// ошибки ввода-вывода или особого типа ошибки безопасности.</exception>
+        /// <exception cref="ArgumentNullException">Исключение возникает, если параметр searchPatternList равен null или колличество элементов в нем равно 0.</exception>
+        public void SearchFilesInDirectory(String directory, IEnumerable<string> searchPatternList, Delegate method)
+        {
+            if ((searchPatternList == null) || (searchPatternList.Count<string>() == 0))
+                throw new ArgumentNullException("searchPatternList");
+            foreach (string searchPattern in searchPatternList)
+            {
+                using (var iterator = Directory.EnumerateFiles(directory, searchPattern).GetEnumerator())
+                {
+                    try
+                    {
+                        while (iterator.MoveNext())
+                            Application.Current.Dispatcher.BeginInvoke(method, iterator.Current);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Debug.Print(ex.Message);
+                    }
+                    catch (DirectoryNotFoundException ex)
+                    {
+                        Debug.Print(ex.Message);
+                    }
+                    catch (IOException ex)
+                    {
+                        Debug.Print(ex.Message);
+                    }
+                    catch (UnauthorizedAccessException ex)
+                    {
+                        Debug.Print(ex.Message);
+                    }
+                }
+            }
+            using (var iterator = Directory.EnumerateDirectories(directory).GetEnumerator())
+            {
+                while (iterator.MoveNext())
+                {
+                    try
+                    {
+                        SearchFilesInDirectory(iterator.Current, searchPatternList, method);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Debug.Print(ex.Message);
+                    }
+                    catch (DirectoryNotFoundException ex)
+                    {
+                        Debug.Print(ex.Message);
+                    }
+                    catch (IOException ex)
+                    {
+                        Debug.Print(ex.Message);
+                    }
+                    catch (UnauthorizedAccessException ex)
+                    {
+                        Debug.Print(ex.Message);
+                    }
+                }
+            }
+        }
+ */
+
+/**
+ /// <summary>
+    /// Предоставляет методы для поиска файлов в директории, включая вложенные.
+    /// </summary>
+    internal class FileWalker
+    {
+        #region Ctor
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса FileWalker.
+        /// </summary>
+        /// <param name="includePath">Список включаемых путей.</param>
+        /// <param name="excludePath">Список исключаемых путей.</param>
+        /// <param name="includeExtension">Список включаемых расширений.</param>
+        /// <param name="excludeExtension">Список исключаемых расширений.</param>
+        public FileWalker(IEnumerable<String> includePath, IEnumerable<String> excludePath, IEnumerable<String> includeExtension, IEnumerable<String> excludeExtension)
+        {
+            IncludePath = includePath;
+            ExcludePath = excludePath;
+            IncludeExtension = includeExtension;
+            ExcludeExtension = excludeExtension;
+        }
+        
+#if DEBUG
+        /// <summary>
+        /// Полезен для обеспечения того, чтобы объекты должным образом были обработаны сборщиком мусора.
+        /// </summary>
+        ~FileWalker()
+        {
+            Debug.WriteLine(string.Format("{0} ({1}) Finalized", GetType().Name, GetHashCode()));
+        }
+#endif
+        
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Возвращает или задает список включаемых путей.
+        /// </summary>
+        private IEnumerable<String> IncludePath { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает список исключаемых путей.
+        /// </summary>
+        private IEnumerable<String> ExcludePath { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает список включаемых расширений.
+        /// </summary>
+        private IEnumerable<String> IncludeExtension { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает список исключаемых расширений.
+        /// </summary>
+        private IEnumerable<String> ExcludeExtension { get; set; }
+
+        #endregion
+
+        #region Public Members
+
+        /// <summary>
+        /// Возвращает имена файлов (включая пути) из указанного каталога.
+        /// </summary>
+        /// <returns>Массив String содержит имена файлов из указанного каталога.Имена файлов включают полный путь к файлу.</returns>
+        public IEnumerable<String> GetFiles()
+        {
+            // Создаем список
+            var list = new List<String>();
+            // Проверяем список включаемых папок
+            if (IncludePath != null)
+            {
+                using (var enumerator = IncludePath.GetEnumerator())
+                {
+                    // Проходим все пути
+                    while (enumerator.MoveNext())
+                    {
+                        // Проверяем список строк поиска
+                        if (IncludeExtension != null)
+                        {
+                            using (var iterator = IncludeExtension.GetEnumerator())
+                            {
+                                // Проходим все строки поиска
+                                while (iterator.MoveNext())
+                                {
+                                    // Проверяем папку в исключенных
+                                    if (!ExcludePath.Contains(enumerator.Current, StringComparer.OrdinalIgnoreCase))
+                                    {
+                                        // Получаем файлы
+                                        list.AddRange(GetFiles(enumerator.Current, iterator.Current));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            // Возвращаем результат
+            return list;
+        }
+
+        #endregion
+
+        #region Private Members
+
+        /// <summary>
+        /// Возвращает имена файлов (включая пути) из указанного каталога, отвечающие условиям заданного шаблона поиска.
+        /// </summary>
+        /// <param name="directory">Каталог, в котором необходимо выполнить поиск.</param>
+        /// <param name="searchPattern">Строка поиска, которую необходимо сравнить с именами файлов, на которые указывает путь</param>
+        /// <returns>Массив String содержит имена файлов из указанного каталога, отвечающие условиям заданного шаблона поиска.Имена файлов включают полный путь к файлу.</returns>
+        private IEnumerable<String> GetFiles(String directory, String searchPattern)
+        {
+            // Создаем список
+            var list = new List<String>();
+            // Итератор по файлам
+            using (var iterator = Directory.EnumerateFiles(directory, searchPattern).GetEnumerator())
+            {
+                try
+                {
+                    while (iterator.MoveNext())
+                    {
+                        // Получаем расширение файла и проверяем его
+                        var fileInfo = new FileInfo(iterator.Current);
+                        if (!ExcludeExtension.Contains(String.Concat("*", fileInfo.Extension)))
+                        {
+                            // Добавляем в список
+                            list.Add(iterator.Current);
+                        }
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    // Путь является строкой нулевой длины, содержит только пробелы или содержит недопустимые символы.
+                    // Параметр поиска не является допустимым значением.
+                    Debug.Print(ex.Message);
+                }
+                catch (DirectoryNotFoundException ex)
+                {
+                    // Путь недопустим, например, ссылается на неподключенный диск.
+                    Debug.Print(ex.Message);
+                }
+                catch (IOException ex)
+                {
+                    // Путь является именем файла.
+                    // Длина указанного пути, имени файла или обоих параметров превышает установленный системой предел.
+                    Debug.Print(ex.Message);
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    // Исключение, возникающее в случае запрета доступа операционной системой 
+                    // из-за ошибки ввода-вывода или особого типа ошибки безопасности.
+                    Debug.Print(ex.Message);
+                }
+            }
+            // Итератор по директориям
+            using (var iterator = Directory.EnumerateDirectories(directory).GetEnumerator())
+            {
+                while (iterator.MoveNext())
+                {
+                    try
+                    {
+                        // Проверяем папку в исключенных
+                        if (!ExcludePath.Contains(iterator.Current, StringComparer.OrdinalIgnoreCase))
+                        {
+                            list.AddRange(GetFiles(iterator.Current, searchPattern));
+                        }
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        // Путь является строкой нулевой длины, содержит только пробелы или содержит недопустимые символы.
+                        // Параметр поиска не является допустимым значением.
+                        Debug.Print(ex.Message);
+                    }
+                    catch (DirectoryNotFoundException ex)
+                    {
+                        // Путь недопустим, например, ссылается на неподключенный диск.
+                        Debug.Print(ex.Message);
+                    }
+                    catch (IOException ex)
+                    {
+                        // Путь является именем файла.
+                        // Длина указанного пути, имени файла или обоих параметров превышает установленный системой предел.
+                        Debug.Print(ex.Message);
+                    }
+                    catch (UnauthorizedAccessException ex)
+                    {
+                        // Исключение, возникающее в случае запрета доступа операционной системой 
+                        // из-за ошибки ввода-вывода или особого типа ошибки безопасности.
+                        Debug.Print(ex.Message);
+                    }
+                }
+            }
+            // Возвращаем результат
+            return list;
+        }
+
+        #endregion
+    }
+ * /
